b7e71815071ba971e5941f5e8031ffdf2f74e932
Thigo De Morais <demoraisthiago63@gmail.com>
2023-05-29 17:16:48 -0300

    About view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,42 +1,68 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
-	
-	@FXML
-	private MenuItem menuItemAlternativa; 
-	
-	@FXML
-	private MenuItem menuItemPergunta;
-	
-	@FXML
-	private MenuItem menuItemSobre;
 
 	@FXML
-	public void onMenuItemAlternativaAction() {
-		System.out.println("onMenuItemAlternativaAction");
-	}
-	
+	private MenuItem menuItemAlternative;
+
 	@FXML
-	public void onMenuItemPerguntaAction() {
-		System.out.println("onMenuItemPerguntaAction");
+	private MenuItem menuItemQuestion;
+
+	@FXML
+	private MenuItem menuItemAbout;
+
+	@FXML
+	public void onMenuItemAlternativeAction() {
+		System.out.println("menuItemAlternative");
+	}
+
+	@FXML
+	public void onMenuItemQuestionAction() {
+		System.out.println("menuItemQuestion");
 
 	}
-	
-	public void onMenuItemSobreAction() {
-		System.out.println("onMenuItemSobreAction");
 
+	public void onMenuItemAboutAction() {
+		loadView("/gui/About.fxml");
 	}
-	
+
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		// TODO Auto-generated method stub
-		
-	}	
+
+	}
+
+	private void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error Loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 }
