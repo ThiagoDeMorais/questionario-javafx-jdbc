7b13b7666be43602d3c29a47034a619ce2306cbc
Thigo De Morais <demoraisthiago63@gmail.com>
2023-05-30 16:01:57 -0300

    QuestionForm

--- src/gui/QuestionListController.java
+++ src/gui/QuestionListController.java
@@ -1,44 +1,54 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Question;
 import model.servicies.QuestionService;
 
-public class QuestionListController implements Initializable{
+public class QuestionListController implements Initializable {
 	private QuestionService service;
-	
+
 	@FXML
 	private TableView<Question> tableViewQuestion;
-	
+
 	@FXML
-	private TableColumn<Question,Integer> tableColumnId;
-	
+	private TableColumn<Question, Integer> tableColumnId;
+
 	@FXML
-	private TableColumn<Question,String> tableColumnDescription;
-	
+	private TableColumn<Question, String> tableColumnDescription;
+
 	@FXML
 	private Button btNew;
-	
+
 	private ObservableList<Question> obsList;
-	
+
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.curretStage(event);
+		createDialogForm("/gui/QuestionForm.fxml", parentStage);
 	}
-	
+
 	public void setQuestionService(QuestionService service) {
 		this.service = service;
 	}
@@ -51,18 +61,36 @@ public class QuestionListController implements Initializable{
 	private void initializeNodes() {
 		tableColumnId.setCellValueFactory((new PropertyValueFactory<>("id")));
 		tableColumnDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
-		
+
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewQuestion.prefHeightProperty().bind(stage.heightProperty());
 	}
-	
+
 	public void updateTableView() {
-		if(service == null) {
+		if (service == null) {
 			throw new IllegalStateException("Serviço nulo");
 		}
-		
+
 		List<Question> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewQuestion.setItems(obsList);
+	}
+
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Entre com o enunciado da questão");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
