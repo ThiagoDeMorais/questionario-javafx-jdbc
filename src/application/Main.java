d9b8842412a8290b9a69dbde42ee95d10676f674
Marcos Felipe <mf7778838@gmail.com>
2023-06-01 17:52:56 -0300

    Estilização MainView

--- src/application/Main.java
+++ src/application/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
 			
 			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
-			primaryStage.setTitle("Sample JavaFX application");
+			primaryStage.setTitle("Projeto Integrador II-2");
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
